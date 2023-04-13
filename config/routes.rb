84a6ffa3a4dfdc3164a1f66b7e4eeff2d190071b
hashimoto <126651146+kazuyukijob@users.noreply.github.com>
2023-04-13 09:23:52 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   root to: 'homes#top'
   get 'relationships/followings'
   get 'relationships/followers'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- devise_for :users
- root to: 'homes#top'
-   get 'home/about' => 'homes#about'
+  devise_for :users
+  get 'home/about' => 'homes#about'
+  get "search" => "searches#search"
 
 
  resources :users, only: [:show, :edit, :update, :index]
-  resources :books, only: [:create, :index, :show, :destroy, :edit, :updatey] do
-    resource :favorites, only: [:create, :destroy]
-    resources :book_comments, only: [:create, :destroy]
+ resources :books, only: [:create, :index, :show, :destroy, :edit, :updatey] do
+ resource :favorites, only: [:create, :destroy]
+ resources :book_comments, only: [:create, :destroy]
   end
 
   resources :users, only:[:index, :show, :edit, :update] do
