d690d8576556699da112f10d4e5d021faa0b5ba4
hashimoto <126651146+kazuyukijob@users.noreply.github.com>
2023-04-30 09:37:11 +0000

    komment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,12 +1,20 @@
 class BookCommentsController < ApplicationController
 
-  def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
-  end
+def create
+   book = Book.find(params[:book_id])
+   comment = current_user.book_comments.new(book_comment_params)
+   comment.book_id = book.id
+   comment.save
+   @books=Book.find(params[:book_id])
+   @book_comment = BookComment.new
+end
+
+ def destroy
+  @books=Book.find(params[:book_id])
+  @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
+  @book_comment.destroy
+  @book_comment = BookComment.new
+ end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   root to: 'homes#top'
+  root to: 'homes#top'
   get 'relationships/followings'
   get 'relationships/followers'
   devise_for :users
@@ -8,16 +7,16 @@ Rails.application.routes.draw do
   get "search" => "searches#search"
 
 
- resources :users, only: [:show, :edit, :update, :index]
- resources :books, only: [:create, :index, :show, :destroy, :edit, :updatey] do
- resource :favorites, only: [:create, :destroy]
- resources :book_comments, only: [:create, :destroy]
-  end
+  resources :users, only: [:show, :edit, :update, :index]
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :updatey] do
+  resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
+    end
 
   resources :users, only:[:index, :show, :edit, :update] do
-    member do
-      get :follows, :followers
-    end
-    resource :relationships, only: [:create, :destroy]
+  member do
+  get :follows, :followers
+      end
+  resource :relationships, only: [:create, :destroy]
   end
 end
